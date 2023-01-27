35e5c1a9ff2b9f21ebb7dd4f975e3df056e319d6
shahimsha786 <121596739+shahimsha786@users.noreply.github.com>
2023-01-27 11:21:36 +0530

    my first commit add

---
+++ Additionof2.java
@@ -0,0 +1,15 @@
+package com.add;
+
+public class Additionof2 {
+
+	public int add2(int x, int y) {
+		return x+y;
+	}
+	public static void main(String[] args) {
+		Additionof2 add2=new Additionof2();
+		int z=add2.add2(4225, 2229);
+		System.out.println("addition of two numbers is  "+z);
+
+	}
+
+}
